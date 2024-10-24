760247c8581db1c3f7546976dd5f1798ef511875
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2024-10-24 14:31:57 +0800

    Added helper for handling decimal places.

---
+++ src/utils/roundTo.ts
@@ -0,0 +1,21 @@
+/** Handle both positive and negative with decimal.
+ *
+ * Modified to cater `TypeScript`,
+ * _Reference [here](https://stackoverflow.com/questions/15762768/javascript-math-round-to-two-decimal-places)._ */
+export default function roundTo(n: number, digits?: number) {
+  let negative = false;
+  if (digits === undefined) {
+    digits = 0;
+  }
+  if (n < 0) {
+    negative = true;
+    n = n * -1;
+  }
+  let multiplicator = Math.pow(10, digits);
+  n = parseFloat((n * multiplicator).toFixed(11));
+  n = +(Math.round(n) / multiplicator).toFixed(digits);
+  if (negative) {
+    n = +(n * -1).toFixed(digits);
+  }
+  return n;
+}
