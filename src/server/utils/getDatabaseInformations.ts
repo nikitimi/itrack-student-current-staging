3483539b71bb66a7df36a4a6d53e0d1b95b24ff4
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2024-11-02 12:01:51 +0800

    - Optimize code for getting database information.

--- src/server/utils/getDatabaseInformations.ts
+++ src/server/utils/getDatabaseInformations.ts
@@ -3,36 +3,60 @@
 import type { MongoExtra } from '@/lib/schema/mongoExtra';
 import type GradeInfo from '@/utils/types/gradeInfo';
 
-import { headers } from 'next/headers';
-
-import { HEADER_KEY } from '@/utils/constants';
 import { BaseAPIResponse } from '../lib/schema/apiResponse';
+import { Certificate } from '@/lib/enums/certificate';
+import fetchHelper from '@/server/utils/fetch';
+import { APIRoutes } from '../lib/enum/apiRoutes';
 
-async function getDatabaseInformations(studentNumber: string) {
+type ResponseInArray = ((GradeInfo | Certificate) & MongoExtra)[];
+type GetDatabaseInformation = {
+  grades: (GradeInfo & MongoExtra)[];
+  certificate: Certificate[];
+};
+
+async function getDatabaseInformations(
+  studentNumber: string
+): Promise<GetDatabaseInformation> {
   try {
-    const headerList = headers();
-    const origin = headerList.get(HEADER_KEY.origin) as string;
+    const apiRoutes: APIRoutes[] = [
+      '/api/mongo/grades',
+      '/api/mongo/certificate',
+      // '/api/mongo/internship'
+    ] as const;
+    let grades: GetDatabaseInformation['grades'] = [];
+    let certificate: GetDatabaseInformation['certificate'] = [];
 
-    const url = new URL(
-      '/api/mongo/grades?' + new URLSearchParams({ studentNumber }),
-      origin
-    );
+    for (const api of apiRoutes) {
+      const response = await fetchHelper(api, 'GET', {
+        studentNumber,
+      });
+      const { data, errorMessage } =
+        (await response.json()) as BaseAPIResponse<ResponseInArray>;
 
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const { data } = (await response.json()) as BaseAPIResponse<
-      (GradeInfo & MongoExtra)[]
-    >;
-    return data;
+      if (errorMessage.length > 0) throw new Error(errorMessage[0]);
+
+      switch (true) {
+        case api.includes('certificate'):
+          certificate = Object.entries(data[0])
+            .filter(([key]) => !isNaN(parseInt(key, 10)))
+            .map(([, v]) => v as Certificate);
+          break;
+        case api.includes('grades'):
+          grades = data as GetDatabaseInformation['grades'];
+          break;
+        case api.includes('internship'):
+          break;
+      }
+    }
+
+    return {
+      grades,
+      certificate,
+    };
   } catch (e) {
     const error = e as Error;
     console.log(error.message);
-    const data = [] as (GradeInfo & MongoExtra)[];
-    return data;
+    return { certificate: [], grades: [] };
   }
 }
 export default getDatabaseInformations;
